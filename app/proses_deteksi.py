f4f169a1e24287265c5140508f5554a25aa4a65f
rixkyduwi <55983435+rixkyduwi@users.noreply.github.com>
2024-07-25 07:09:10 +0700

    update
    Co-Authored-By: zulfatunnisa <80380143+zulfatunnisa@users.noreply.github.com>

--- app/proses_deteksi.py
+++ app/proses_deteksi.py
@@ -8,30 +8,6 @@ from flask import Flask, jsonify, request, session
 from . import app,db,History
 from PIL import Image
 from io import BytesIO
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-# Get the base directory (root of the project)
-base_dir = os.path.abspath(os.path.dirname(__file__))
-
-# Set the path to the yolo executable
-yolo_path = os.path.join(base_dir, 'env', 'bin', 'yolo')
-
-# Set the environment variable PATH
-os.environ['PATH'] += os.pathsep + yolo_path
-
-# Set environment variables for matplotlib and ultralytics
-matplotlib_cache_dir = os.path.join(base_dir, 'env', 'matplotlib_cache')
-ultralytics_config_dir = os.path.join(base_dir, 'env', 'Ultralytics_config')
-
-os.environ['MPLCONFIGDIR'] = matplotlib_cache_dir
-os.environ['YOLO_CONFIG_DIR'] = ultralytics_config_dir
-
-print("Updated PATH:", os.environ['PATH'])
-print("MPLCONFIGDIR:", os.environ['MPLCONFIGDIR'])
-print("YOLO_CONFIG_DIR:", os.environ['YOLO_CONFIG_DIR'])
 
 # Load the model
 project_directory = os.path.abspath(os.path.dirname(__file__))
